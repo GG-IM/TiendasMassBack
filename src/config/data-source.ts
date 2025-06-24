1f1d0e179c9ea9d58cadd9c65f52ec66f05ea1a2
Jim Fernandez <n00316076@upn.pe>
2025-06-24 14:15:57 -0500

    Update data-source.ts

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,15 +1,20 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "123456789",
-  database: "tiendasmass",
-  synchronize:false,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: false, // TRUE solo si quieres auto-crear las tablas, pero mejor manejar migraciones
   logging: true,
+  ssl: {
+    rejectUnauthorized: true,
+  },
   entities: ["src/entities/**/*.ts"],
 });
